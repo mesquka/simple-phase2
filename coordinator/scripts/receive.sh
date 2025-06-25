81122c046661cc7dee8434ea5db65a0b54b6e8c0
Kieran Test <me@mesquka.com>
2025-06-25 01:05:50 +0000

    Add support for custom transit relay

--- coordinator/scripts/receive.sh
+++ coordinator/scripts/receive.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+TRANSIT_RELAY="tcp:magic-wormhole-transit.debian.net:4001"
+
 CONTRIBUTION_NUMBER="$(NODE_NO_WARNINGS=1 /scripts/getNextContributionNumber.js)"
 
 echo "Enter response code:"
 read RESPONSE_CODE
 
-wormhole receive --output-file /workspace/contributions/"$CONTRIBUTION_NUMBER" --accept-file "$RESPONSE_CODE"
+wormhole --transit-helper="$TRANSIT_RELAY" receive --output-file /workspace/contributions/"$CONTRIBUTION_NUMBER" --accept-file "$RESPONSE_CODE"
 
 echo "Transcript hash:"
 shasum --algorithm 256 /workspace/contributions/"$CONTRIBUTION_NUMBER"/transcript.json
--- coordinator/scripts/send.sh
+++ coordinator/scripts/send.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+TRANSIT_RELAY="tcp:magic-wormhole-transit.debian.net:4001"
+
 CONTRIBUTION_NUMBER="$(NODE_NO_WARNINGS=1 /scripts/getCurrentContributionNumber.js)"
 
 echo
 echo "SEND WORMHOLE CHALLENGE CODE TO CONTRIBUTOR:"
 echo
 
-wormhole send /workspace/contributions/"$CONTRIBUTION_NUMBER"
+wormhole --transit-helper="$TRANSIT_RELAY" send /workspace/contributions/"$CONTRIBUTION_NUMBER"
